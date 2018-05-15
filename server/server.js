9f78fe4066e7e55ff81e4320048f25d7f838410b
unknown <craigericlewis@gmail.com>
2018-05-15 11:43:47 -0400

    fix

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const {ObjectID} = require('mongodb');
+var {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require ('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/User');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
