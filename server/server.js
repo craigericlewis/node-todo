99fd99e42a343bcd12c25388965425f527e5535d
craigericlewis <craiglewis1508@yahoo.co.in>
2018-05-15 18:38:12 -0400

    fixed conflicts

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require ('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/User');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -40,7 +40,7 @@ app.get('/todos/:id', (req, res) => {
 
   Todo.findById(id).then((todo)=>{
     if(!todo){
-      retrun res.status(404).send();
+      return res.status(404).send();
     }
     res.send(todo);
   }).catch((e) => {
