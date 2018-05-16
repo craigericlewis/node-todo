af1d99d0a8efbda31fcb18d617beee17dd122a5c
unknown <craigericlewis@gmail.com>
2018-05-16 17:02:22 -0400

    something

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
-var {ObjectID} = require('mongodb');
+var {
+  ObjectID
+} = require('mongodb');
+const = _require('lodash')
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require ('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+  mongoose
+} = require('./db/mongoose');
+var {
+  Todo
+} = require('./models/todo');
+var {
+  User
+} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -25,7 +34,9 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos})
+    res.send({
+      todos
+    })
   }, (e) => {
     res.status(400).send(e);
   });
@@ -34,12 +45,12 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)){
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findById(id).then((todo)=>{
-    if(!todo){
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
     res.send(todo);
@@ -49,8 +60,29 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+});
+
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
-module.exports = {app};
+module.exports = {
+  app
+};
